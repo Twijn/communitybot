fe3a42136947223f19414c80f9addaf8515a7689
Tyler Twining <twiningtyler@gmail.com>
2020-10-19 18:35:06 -0500

    Finish implementation of setprefix

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,17 +1,27 @@
+const cache = require("../internal/cache");
+
 const command = {
     name: 'setprefix'
     , description: 'Set the prefix for the current server'
-    , usage: `{{prefix}}setprefix <Prefix>`
+    , usage: `setprefix <Prefix>`
     , execute(message, args) {
-        if (args.length > 0) {
-            if (args[0].length <= 2) {
-
+        cache.guild.get(message.guild.id, (err, guild) => {
+            if (args.length > 0) {
+                if (args[0].length <= 4) {
+                    cache.guild.update(guild.id, {prefix: args[0].toLowerCase()}, (success, err) => {
+                        if (success) {
+                            message.reply(`CommunityBot prefix set to \`${args[0].toLowerCase()}\``)
+                        } else {
+                            message.reply(`An error occurred! ${err}`);
+                        }
+                    });
+                } else {
+                    message.reply(`Prefix must be 4 characters or less in length!`);
+                }
             } else {
-                message.channel.send(`<@${message.author.id}> Prefix must be 2 characters or less in length!`);
+                message.reply(`Invalid arguments! \`${(guild === undefined || guild === null ? '!' : guild.prefix) + command.usage.replace("{{prefix}}")}\``);
             }
-        } else {
-            message.channel.send(`<@${message.author.id}> Invalid arguments! \`${command.usage.replace("{{prefix}}", message.guild.prefix === undefined ? '!' : message.guild.prefix)}\``);
-        }
+        });
     }
 };
 
