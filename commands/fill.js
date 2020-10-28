9b2a9ee0cbe446dcde47163d4e42545b7f222000
Tyler Twining <twiningtyler@gmail.com>
2020-10-27 23:25:32 -0500

    Add very basic fill command.

---
+++ commands/fill.js
@@ -0,0 +1,19 @@
+const qm = require("../internal/queuemanager");
+const con = require("../internal/database");
+const Discord = require("discord.js");
+
+const command = {
+    name: 'fill'
+    , description: 'Fill the game with the next people in the queue'
+    , permission: 'MANAGE_CHANNELS'
+    , usage: `fill`
+    , execute(message, args) {
+        qm.autoAdd(message.guild, 100, (err, members) => {
+            if (err) {message.reply(err);console.error(err);return;}
+
+            
+        });
+    }
+};
+
+module.exports = command;
