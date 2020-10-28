aa6ed6d8aad8f9df50e9944bba8bfe6f4de6a1ad
Tyler Twining <twiningtyler@gmail.com>
2020-10-27 23:25:54 -0500

    Update cache internal library

--- internal/cache.js
+++ internal/cache.js
@@ -8,10 +8,6 @@ const con = require("../internal/database");
 const cache = {
     guild: {
         get: (id, callback) => {
-            if (guilds.hasOwnProperty(id)) {
-                callback(null, guilds[id]);
-            }
-
             con.query("select * from guilds where id = ?;", [id], (err, result) => {
                 if (err) {
                     callback(err, null);
@@ -19,7 +15,6 @@ const cache = {
                 }
 
                 if (result.length > 0) {
-                    guilds[id] = result[0];
                     callback(null, result[0]);
                 } else {
                     callback("Guild was not found!", null);
@@ -28,6 +23,73 @@ const cache = {
         }
         , add: (id, url, prefix = "!") => {
             con.query("insert into guilds (id, url, prefix) values (?, ?, ?);", [id, url, prefix]);
+        }
+        , update: (id, values, callback) => {
+            cache.guild.get(id, (err, guild) => {
+                if (err) {console.log(err);callback(false, err);return;}
+
+                let updateQuery = "";
+                let params = [];
+
+                for (const index in values) {
+                    let value = values[index];
+
+                    if (guild.hasOwnProperty(index) && index !== "id") {
+                        guild[index] = value;
+
+                        if (updateQuery !== "") {
+                            updateQuery += ", ";
+                        }
+                        updateQuery += `${index} = ?`;
+
+                        params = [
+                            ...params
+                            , value
+                        ];
+                    }
+                }
+
+                if (params.length > 0) { // make sure we actually have to update
+                    params = [
+                        ...params
+                        , id
+                    ];
+
+                    con.query(`update guilds set ${updateQuery} where id = ?;`, params, err => {
+                        if (err) {console.log(err);callback(false, err);return;}
+
+                        callback(true);
+                    });
+                } else {
+                    callback(true);
+                }
+            });
+        }
+        , queue: {
+            reserved: {
+                add: (guild_id, user_id) => {
+                    con.query("insert into queue (user_id, guild_id, status) values (?, ?, 'reserved') on duplicate key update status = 'reserved';", [user_id, guild_id]);
+                }
+                , remove: (guild_id, user_id) => {
+                    con.query("update queue set status = 'game' where guild_id = ? and user_id = ?;", [guild_id, user_id]);
+                }
+                , get: (guild_id, callback) => {
+                    con.query("select * from queue where guild_id = ? and status = 'reserved';", [guild_id], (err, result) => {
+                        if (err) {callback(err, null);return;}
+        
+                        let resultArray = [];
+        
+                        result.forEach(seat => {
+                            resultArray = [
+                                ...resultArray
+                                , seat.user_id
+                            ];
+                        });
+        
+                        callback(null, resultArray);
+                    });
+                }
+            }
         }
     }
 }
