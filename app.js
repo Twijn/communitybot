39a672b26e67a9fc090da1c0dd3ee840b8a1d923
Tyler Twining <twiningtyler@gmail.com>
2020-10-28 04:14:54 -0500

    Add Twitch bot

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // Start Discord Bot
 console.log("Starting Discord Bot!");
 require("./discord");
+
+// Start Twitch Bot
+console.log("Starting Twitch Bot!");
+require("./twitch");
---
+++ twitch.js
@@ -0,0 +1,22 @@
+const TwitchBot = require("twitch-bot");
+const config = require("./config.json");
+
+const Bot = new TwitchBot({
+    username: config.twitch.username,
+    oauth: config.twitch.oauth,
+    channels: config.twitch.channels
+})
+
+Bot.on('join', channel => {
+    console.log(`Joined channel: ${channel}`);
+})
+
+Bot.on('error', err => {
+    console.error(err);
+})
+
+Bot.on('message', chatter => {
+    if (chatter.message === "!test") {
+        Bot.say("Command executed! PogChamp");
+    }
+})
