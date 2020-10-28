a9de1e0053bb78413bb18cf78b9b2368b0489bf1
Tyler Twining <twiningtyler@gmail.com>
2020-10-27 23:20:24 -0500

    Add voice lobby change detection

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ client.commands = new Discord.Collection();
 const config = require("./config.json");
 const con = require("./internal/database");
 const cache = require("./internal/cache");
+const qm = require("./internal/queuemanager");
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -17,7 +18,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     console.log(`I am ready! Logged in as ${client.user.tag}!`);
-	console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`); 
+    console.log(`Bot has started with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
 });
 
 // implement commands
@@ -50,20 +51,106 @@ client.on('message', message => {
         
     
         if (!client.commands.has(command)) return;
-    
-        try {
-            client.commands.get(command).execute(message, args);
-        } catch (error) {
-            console.error(error);
-            message.reply('There was an error trying to execute that command!');
+
+        let cmd = client.commands.get(command);
+        
+        if (!cmd.hasOwnProperty("permission") || message.guild.member(message.author.id).hasPermission(cmd.permission)) {
+            try {
+                cmd.execute(message, args);
+            } catch (error) {
+                console.error(error);
+                message.reply('There was an error trying to execute that command!');
+            }
+        } else {
+            message.reply(`Insufficient permission! You need \`${cmd.permission}\``);
         }
     });
 });
 
+const findChannel = (guild, name, callback = () => {}) => {
+    con.query("select id from channels where name = ? and guild_id = ?;", [name, guild.id], (err, result) => {
+        if (err) {console.error(err);return;}
+
+        if (result.length > 0) {
+            let channelId = result[0].id;
+            let channel = guild.channels.cache.find(channel => channel.id === channelId);
+
+            callback(channel);
+        }
+    });
+}
+
+const findRole = (guild, name, callback = () => {}) => {
+    con.query("select id from roles where name = ? and guild_id = ?;", [name, guild.id], (err, result) => {
+        if (err) {console.error(err);return;}
+
+        if (result.length > 0) {
+            let roleId = result[0].id;
+            let role = guild.roles.cache.find(role => role.id === roleId);
+
+            callback(role);
+        }
+    });
+}
+
 client.on("voiceStateUpdate", (oldState, newState) => {
     if (oldState.channelID !== newState.channelID) {
+        let user = newState.guild.member(newState.id);
+
+        let fromChannel = oldState.guild.channels.cache.find(channel => channel.id === oldState.channelID);
+        let toChannel = newState.guild.channels.cache.find(channel => channel.id === newState.channelID);
+
+        let guild = newState.guild;
         
+        if (!fromChannel || !toChannel || fromChannel.id !== toChannel.id) {
+            const processChannelQueue = channel => {
+                if (toChannel && channel.id === toChannel.id) {
+                    qm.addPlayerToQueue(guild, user);
+                }
+            }
+
+            findChannel(guild, "Queue", processChannelQueue);
+            findChannel(guild, "Silent Queue", processChannelQueue);
+
+            const processChannelLeave = channel => {
+                if (toChannel && channel.id === toChannel.id) {
+                    findRole(guild, "In Queue", inQueueRole => {
+                        if (user.roles.cache.find(role => role.id === inQueueRole.id)) {
+                            user.roles.remove(inQueueRole);
+
+                            const leaveQueue = new Discord.MessageEmbed()
+                                .setColor('#0099ff')
+                                .setTitle('Left Queue!')
+                                .setDescription(`You left the community game queue in \`${guild.name}\``);
+
+                            user.send(leaveQueue);
+                        }
+                    });
+                    findRole(guild, "In Game", inGameRole => {
+                        if (user.roles.cache.find(role => role.id === inGameRole.id)) {
+                            user.roles.remove(inGameRole);
+                            
+                            const leaveQueue = new Discord.MessageEmbed()
+                                .setColor('#0099ff')
+                                .setTitle('Left Game!')
+                                .setDescription(`You left the community game in \`${guild.name}\``);
+
+                            user.send(leaveQueue);
+                        }
+                    });
+                    
+                    user.voice.setChannel(null);
+
+                    con.query("delete from queue where user_id = ? and guild_id = ?;", [user.id, guild.id]);
+                }
+            }
+
+            findChannel(guild, "Exit Game / Leave Queue", processChannelLeave);
+        } else {
+
+        }
     }
 });
 
+console.log("Logging in...");
 client.login(config.token);
