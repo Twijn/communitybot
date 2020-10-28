c07fbe5ffde7f8ef2a9dd8c87484cada168efc50
Tyler Twining <twiningtyler@gmail.com>
2020-10-27 23:29:20 -0500

    Add base setting command.

---
+++ commands/setting.js
@@ -0,0 +1,29 @@
+const cache = require("../internal/cache");
+
+const command = {
+    name: 'setting'
+    , description: 'View/set settings for the server'
+    , usage: `setting <Node> [Value]`
+    , permission: 'MANAGE_CHANNELS'
+    , execute(message, args) {
+        cache.guild.get(message.guild.id, (err, guild) => {
+            if (args.length > 0) {
+                if (args[0].length <= 4) {
+                    cache.guild.update(guild.id, {prefix: args[0].toLowerCase()}, (success, err) => {
+                        if (success) {
+                            message.reply(`CommunityBot prefix set to \`${args[0].toLowerCase()}\``)
+                        } else {
+                            message.reply(`An error occurred! ${err}`);
+                        }
+                    });
+                } else {
+                    message.reply(`Prefix must be 4 characters or less in length!`);
+                }
+            } else {
+                message.reply(`Invalid arguments! \`${(guild === undefined || guild === null ? '!' : guild.prefix) + command.usage.replace("{{prefix}}")}\``);
+            }
+        });
+    }
+};
+
+module.exports = command;
