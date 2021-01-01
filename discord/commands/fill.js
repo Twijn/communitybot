26249026fa01d9c69b7e96ed4558cf76c4c5c459
Tyler Twining <twiningtyler@gmail.com>
2020-12-31 23:12:35 -0600

    Base code for !fill

--- discord/commands/fill.js
+++ discord/commands/fill.js
@@ -8,10 +8,63 @@ const command = {
     , permission: 'MANAGE_CHANNELS'
     , usage: `fill`
     , execute(message, args) {
-        qm.autoAdd(message.guild, 100, (err, members) => {
-            if (err) {message.reply(err);console.error(err);return;}
+        let fillAmount = 100;
 
-            
+        if (fillAmount.length > 0 && !isNaN(parseInt(args[0]))) {
+            fillTo = parseInt(args[0]);
+        }
+
+        qm.autoAdd(message.guild, fillAmount, (err, members) => {
+            if (err) {message.reply("An error occurred.");console.error(err);return;}
+
+            let addedMembersString = "";
+            let waitingMembersString = "";
+
+            let addedMembers = [];
+            let waitingMembers = [];
+
+            members.forEach(member => {
+                if (member.voice.channelID === null || member.voice.channelID === undefined) {
+                    if (waitingMembersString !== "") {
+                        waitingMembersString += "\n";
+                    }
+                    waitingMembersString += member.displayName;
+
+                    waitingMembers = [
+                        ...waitingMembers
+                        , member
+                    ];
+                } else {
+                    if (addedMembersString !== "") {
+                        addedMembersString += "\n";
+                    }
+                    addedMembersString += member.displayName;
+
+                    addedMembers = [
+                        ...addedMembers
+                        , member
+                    ];
+                }
+            });
+
+            if (members.length > 0) {
+                const addedMessage = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle(`${members.length} player${members.length === 1 ? ' was' : 's were'} added into the game!`)
+                    .addFields(
+                        {name: "Added Immediately", value: `\`\`\`\n${addedMembers.length === 0 ? 'No players added immediately' : addedMembersString}\`\`\``, inline: true},
+                        {name: "Waiting for Move", value: `\`\`\`\n${waitingMembers.length === 0 ? 'No players are waiting' : waitingMembersString}\`\`\``, inline: true},
+                    );
+
+                message.channel.send(addedMessage);
+            } else {
+                const addedMessage = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle(`No players were added.`)
+                    .setDescription("The lobby may be full, or there may be no one in the queue. :(");
+
+                message.channel.send(addedMessage);
+            }
         });
     }
 };
