cd45036c4048bc4baa7da801e06a131de01dce17
Tyler Twining <twiningtyler@gmail.com>
2020-12-31 23:14:40 -0600

    Add con.pquery to allow for promise-based queries

--- internal/database.js
+++ internal/database.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql');
 const config = require('../config.json');
 
-const con = mysql.createConnection({
+let con = mysql.createConnection({
     host: config.mysql.host,
     user: config.mysql.username,
     password: config.mysql.password,
     database: config.mysql.database
 });
+
+con.pquery = (sql, binding = null) => {
+    return new Promise((resolve, reject) => {
+        con.query(sql, binding, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+};
 
 module.exports = con;
